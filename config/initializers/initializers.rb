7f86cc53cf26fe94305cab01608a91325d0f6c12
Tabish Saleem <tabish.saleem@nxb.com.pk>
2017-08-10 18:31:09 +0500

    Initializers: Added new role clerk

--- config/initializers/initializers.rb
+++ config/initializers/initializers.rb
@@ -386,6 +386,6 @@ CUSTOM_FIELDS = {
         ]
 }
 
-ROLES = ['admin', 'manager', 'staff']
+ROLES = ['admin', 'manager', 'staff', 'clerk']
 
 ENTITIES = ['Account','Category', 'Client', 'Company', 'CompanyEmailTemplate', 'CompanyEntity', 'Currency', 'EmailTemplate', 'Estimate', 'ExpenseCategory', 'Expense', 'InvoiceLineItem', 'InvoiceTask', 'Invoice', 'Item', 'LineItemTax', 'Log', 'PaymentTerm', 'Payment', 'ProjectTask', 'Project', 'RecurringProfileLineItem', 'RecurringProfile', 'Role', 'SentEmail', 'Staff', 'Task', 'TeamMember']
