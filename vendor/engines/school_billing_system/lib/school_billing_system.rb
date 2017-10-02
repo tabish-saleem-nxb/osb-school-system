70147aa879427a6ed9e6d8d5976cf62da56b6c8c
Tabish Saleem <tabish.saleem@nxb.com.pk>
2017-10-02 16:29:46 +0500

    SImpleInvoice: Removed extra library for parents/students

--- vendor/engines/school_billing_system/lib/school_billing_system.rb
+++ vendor/engines/school_billing_system/lib/school_billing_system.rb
@@ -4,5 +4,4 @@ module SchoolBillingSystem
   require "school_billing_system/date_formats"
   require "school_billing_system/services/invoice/invoice_service"
   require "school_billing_system/services/invoice/invoice_bulk_actions_service"
-  require "school_billing_system/services/parent_client/parent_client_bulk_actions_service"
 end
